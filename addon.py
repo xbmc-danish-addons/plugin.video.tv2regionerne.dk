0d8aa5c0261f3843ca737209a7849e42a65d79b3
twinther <tommy@winther.nu>
2011-08-20 10:13:23 +0200

    Version 1.2.0 - Completely rewritten again to use RSS Podcast feeds   as the solution in version 1.1.0 was too fragile.

--- addon.py
+++ addon.py
@@ -1,71 +1,101 @@
-# coding=utf-8
-import os
 import sys
+import cgi as urlparse
 
-import xbmc
 import xbmcgui
 import xbmcplugin
 import xbmcaddon
 
-import danishaddons
-import danishaddons.web
 
-import sources.tv2nord
-import sources.tv2syd
-import sources.tv2fyn
-import sources.tv2oj
-import sources.tv2lorry
-import sources.tv2midtvest
-import sources.tv2east
-import sources.tv2bornholm
+REGIONS = {
+    30000: 'tv2fyn',
+    30001: 'tv2lorry',
+    30002: 'tv2nord',
+    30003: 'tv2east',
+    30004: 'tv2oj'
+}
 
-REGIONS = [
-    {'name' : 'TV Syd', 'slug' : sources.tv2syd.SLUG},
-    {'name' : 'TV2/Fyn', 'slug' : sources.tv2fyn.SLUG},
-    {'name' : 'TV2/Øst', 'slug' : sources.tv2east.SLUG},
-    {'name' : 'TV2/Nord', 'slug' : sources.tv2nord.SLUG},
-    {'name' : 'TV2/Lorry', 'slug' : sources.tv2lorry.SLUG},
-    {'name' : 'TV2/Midt-Vest', 'slug' : sources.tv2midtvest.SLUG},
-    {'name' : 'TV2/Østjylland', 'slug' : sources.tv2oj.SLUG},
+FEEDS = [
+        (30100, 'rss://www.tv2oj.dk/podcast/1930', 'tv2oj'),
+        (30101, 'rss://www.tv2oj.dk/podcast/2220', 'tv2oj'),
+        (30102, 'rss://www.tv2oj.dk/podcast/goaften', 'tv2oj'),
 
-# todo tv2bornholm bruger rtmp over ssl
-#    {'name' : 'TV2/Bornholm', 'slug' : sources.tv2bornholm.SLUG},
+        (30200, 'rss://www.tv2nord.dk/podcast/1210', 'tv2nord'),
+        (30201, 'rss://www.tv2nord.dk/podcast/1930', 'tv2nord'),
+        (30202, 'rss://www.tv2nord.dk/podcast/2220', 'tv2nord'),
+        (30203, 'rss://www.tv2nord.dk/podcast/plus', 'tv2nord'),
+
+        (30300, 'rss://podcast.tv2east.dk/podcaster?channel=nyheder', 'tv2east'),
+        (30301, 'rss://podcast.tv2east.dk/podcaster?channel=nyheder&subchannel=1210', 'tv2east'),
+        (30302, 'rss://podcast.tv2east.dk/podcaster?channel=nyheder&subchannel=1610', 'tv2east'),
+        (30303, 'rss://podcast.tv2east.dk/podcaster?channel=nyheder&subchannel=1810', 'tv2east'),
+        (30304, 'rss://podcast.tv2east.dk/podcaster?channel=nyheder&subchannel=1930', 'tv2east'),
+        (30305, 'rss://podcast.tv2east.dk/podcaster?channel=nyheder&subchannel=2220', 'tv2east'),
+
+        (30400, 'rss://www.tv2fyn.dk/podcast/1930', 'tv2fyn'),
+        (30401, 'rss://www.tv2fyn.dk/podcast/2220', 'tv2fyn'),
+        (30402, 'rss://www.tv2fyn.dk/podcast/plus', 'tv2fyn'),
+
+        (30500, 'rss://www.tv2lorry.dk/podcast/1930', 'tv2lorry'),
+        (30501, 'rss://www.tv2lorry.dk/podcast/2220', 'tv2lorry'),
+        (30502, 'rss://www.tv2lorry.dk/podcast/lounge', 'tv2lorry'),
+        (30503, 'rss://www.tv2lorry.dk/podcast/dervarengang', 'tv2lorry')
 ]
 
-def showRegions():
-    for r in REGIONS:
-        if r['slug'] is not None:
-            icon = danishaddons.ADDON_PATH + "/resources/logos/%s.png" % r['slug']
-            item = xbmcgui.ListItem(r['name'], iconImage = icon)
-            url = danishaddons.ADDON_PATH + '?slug=' + r['slug']
-            xbmcplugin.addDirectoryItem(danishaddons.ADDON_HANDLE, url, item, True)
+SPECIAL = [
+        (30600, 'rss://www.tv2lorry.dk/podcast/dkr', 'danmarkrundt')
+]
 
-    xbmcplugin.addSortMethod(danishaddons.ADDON_HANDLE, xbmcplugin.SORT_METHOD_LABEL)
-    xbmcplugin.endOfDirectory(danishaddons.ADDON_HANDLE)
+class TV2Regionerne(object):
+    def showRegions(self):
+        for stringId in REGIONS:
+            slug = REGIONS.get(stringId)
+            icon = ADDON.getAddonInfo('path') + "/resources/logos/%s.png" % slug
+            item = xbmcgui.ListItem(ADDON.getLocalizedString(stringId), iconImage=icon)
+            item.setProperty("Fanart_Image", ADDON.getAddonInfo('path') + "/fanart.jpg")
+            url = PATH + '?slug=' + slug
+            xbmcplugin.addDirectoryItem(HANDLE, url, item, True)
+
+        if ADDON.getSetting('show.danmark.rundt') == 'true':
+            self._addFeeds(SPECIAL)
+
+        xbmcplugin.endOfDirectory(HANDLE)
+
+
+    def showFeeds(self, slug):
+        self._addFeeds(FEEDS, slug)
+
+        if ADDON.getSetting('show.danmark.rundt') == 'true' and not PARAMS.has_key('slug'):
+            self._addFeeds(SPECIAL)
+
+        xbmcplugin.endOfDirectory(HANDLE)
+
+    def _addFeeds(self, feeds, region=None):
+        for stringId, feedUrl, slug in feeds:
+            if region is None or region == slug:
+                icon = ADDON.getAddonInfo('path') + "/resources/logos/%s.png" % slug
+                item = xbmcgui.ListItem(ADDON.getLocalizedString(stringId), iconImage=icon)
+                item.setProperty("Fanart_Image", ADDON.getAddonInfo('path') + "/fanart.jpg")
+                xbmcplugin.addDirectoryItem(HANDLE, feedUrl, item, True)
 
 
 if __name__ == '__main__':
-    danishaddons.init(sys.argv)
+    ADDON = xbmcaddon.Addon(id='plugin.video.tv2regionerne.dk')
+    PATH = sys.argv[0]
+    HANDLE= int(sys.argv[1])
+    PARAMS = urlparse.parse_qs(sys.argv[2][1:])
 
-    if danishaddons.ADDON_PARAMS.has_key('slug'):
-        slug = danishaddons.ADDON_PARAMS['slug']
+    regionSlug = None
+    regionName = ADDON.getSetting('show.feeds.for')
+    for regionStringId in REGIONS:
+        if regionName == ADDON.getLocalizedString(regionStringId):
+            regionSlug = REGIONS.get(regionStringId)
+            break
 
-        if slug == sources.tv2nord.SLUG:
-            sources.tv2nord.invoke(danishaddons.ADDON_PARAMS)
-        elif slug == sources.tv2syd.SLUG:
-            sources.tv2syd.invoke(danishaddons.ADDON_PARAMS)
-        elif slug == sources.tv2fyn.SLUG:
-            sources.tv2fyn.invoke(danishaddons.ADDON_PARAMS)
-        elif slug == sources.tv2oj.SLUG:
-            sources.tv2oj.invoke(danishaddons.ADDON_PARAMS)
-        elif slug == sources.tv2lorry.SLUG:
-            sources.tv2lorry.invoke(danishaddons.ADDON_PARAMS)
-        elif slug == sources.tv2midtvest.SLUG:
-            sources.tv2midtvest.invoke(danishaddons.ADDON_PARAMS)
-        elif slug == sources.tv2east.SLUG:
-            sources.tv2east.invoke(danishaddons.ADDON_PARAMS)
-        elif slug == sources.tv2bornholm.SLUG:
-            sources.tv2bornholm.invoke(danishaddons.ADDON_PARAMS)
+    tv2r = TV2Regionerne()
+    if PARAMS.has_key('slug'):
+        tv2r.showFeeds(PARAMS.get('slug')[0])
+    elif regionSlug is not None:
+        tv2r.showFeeds(regionSlug)
     else:
-        showRegions()
+        tv2r.showRegions()
 
